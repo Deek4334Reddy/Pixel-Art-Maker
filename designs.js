5cfa07e638094dd7af47712ebed33c3f63fdc370
Deekshith Reddy <dikshithhreddy@gmail.com>
2017-10-27 13:21:22 +0530

    changed the event-listner to submit instead of click

--- designs.js
+++ designs.js
@@ -1,12 +1,6 @@
-// Select color input
-// Select size input
-
 function makeGrid(width, height) {
   let pixelCanvas, grid;
   pixelCanvas = $('#pixel_canvas');
-  if (pixelCanvas.children().length) {
-    pixelCanvas.empty();
-  }
   for(let i = 0; i < height; i++) {
     grid += "<tr>";
     for (let j = 0; j < width; j++) {
@@ -18,17 +12,30 @@ function makeGrid(width, height) {
   pixelCanvas.css("background-color", "#ffffff");//setting background color for canvas
 };
 
-$(document).ready(function(){
-  $('#submit-btn').click(function(evt) { //event-listner for submit-btn on click
-    let height, width;
-    width = $('#input_width').val();
-    height = $('#input_height').val();
+const sizePicker = document.querySelector("#sizePicker");
+sizePicker.addEventListener("submit", function(evt) {
+	var width = $('#input_width').val();
+	var height = $('#input_height').val();
     evt.preventDefault();
     makeGrid(width, height);
-  });
-
-  $('#pixel_canvas').click(function(evt) {
-    let color = $('#colorPicker').val();
-    $(evt.target).css('background-color', color);//
-  });
 });
+
+$('#pixel_canvas').click(function(e) { 
+//Tried changing the event listner on to the individual cells(td) instead of 
+//table but when left click is pressed the color of the cell doesn't change.
+  let color = $('#colorPicker').val();
+  $(e.target).css('background-color', color);//
+});
+
+
+
+
+
+// $(document).ready(function(){
+//   $('#submit-btn').click(function(evt) { //event-listner for submit-btn on click
+//     // let height, width;
+//     var width = $('#input_width').val();
+//     var height = $('#input_height').val();
+//     evt.preventDefault();
+//     makeGrid(width, height);
+//   });
