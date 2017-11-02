06e435f5fcb9c74ca1815afa7517d4266695c0f1
Deekshith Reddy <dikshithhreddy@gmail.com>
2017-11-02 12:30:24 +0530

    event listner looped through each cell

--- designs.js
+++ designs.js
@@ -1,45 +1,27 @@
+const colorPicker = document.getElementById("colorPicker");
+
+var table = document.getElementById("pixel_canvas");
+
 function makeGrid(width, height) {
   let pixelCanvas, grid;
   pixelCanvas = $('#pixel_canvas');
-  for(let i = 0; i < height; i++) {
-    grid += "<tr>";
-    for (let j = 0; j < width; j++) {
-      grid += "<td></td>";
+  for (let i = 0; i < height; i++) {
+    const row = table.insertRow(i);
+      for (let j = 0; j < width; j++) {
+        const cell = row.insertCell(j);
+        cell.onclick = () => {
+        cell.style.backgroundColor = colorPicker.value;
+      };
     }
-    grid += "</tr>";
-  }// Defining height & width of the table
+  }
   pixelCanvas.html(grid);
   pixelCanvas.css("background-color", "#ffffff");//setting background color for canvas
 };
 
 const sizePicker = document.querySelector("#sizePicker");
 sizePicker.addEventListener("submit", function(evt) {
-	var width = $('#input_width').val();
-	var height = $('#input_height').val();
+  var width = $('#input_width').val();
+  var height = $('#input_height').val();
     evt.preventDefault();
     makeGrid(width, height);
 });
-
-$('#pixel_canvas').click(function(e) { 
-//Tried changing the event listner on to the individual cells(td) instead of 
-//table but when left click is pressed the color of the cell doesn't change.
-  let color = $('#colorPicker').val();
-  $(e.target).css('background-color', color);//
-});
-// previous used eventlistner on click-- changing it to submit eventlistner
-// $(document).ready(function(){
-//   $('#submit-btn').click(function(evt) { //event-listner for submit-btn on click
-//     // let height, width;
-//     var width = $('#input_width').val();
-//     var height = $('#input_height').val();
-//     evt.preventDefault();
-//     makeGrid(width, height);
-//   });
-// $(document).ready(function(){
-//   $('#submit-btn').click(function(evt) { //event-listner for submit-btn on click
-//     // let height, width;
-//     var width = $('#input_width').val();
-//     var height = $('#input_height').val();
-//     evt.preventDefault();
-//     makeGrid(width, height);
-//   });
