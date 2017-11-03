80cfb9223aed8b0d9901bbc29b55d26d0747601d
Deekshith Reddy <dikshithhreddy@gmail.com>
2017-11-03 11:42:27 +0530

    Proper indentation

--- designs.js
+++ designs.js
@@ -3,25 +3,26 @@ const colorPicker = document.getElementById("colorPicker");
 var table = document.getElementById("pixel_canvas");
 
 function makeGrid(width, height) {
-  let pixelCanvas, grid;
-  pixelCanvas = $('#pixel_canvas');
-  for (let i = 0; i < height; i++) {
-    const row = table.insertRow(i);
-      for (let j = 0; j < width; j++) {
-        const cell = row.insertCell(j);
-        cell.onclick = () => {
-        cell.style.backgroundColor = colorPicker.value;
-      };
+    let pixelCanvas, grid;
+    pixelCanvas = $('#pixel_canvas');
+    for (let i = 0; i < height; i++) { //Defining the height of the grid
+        const row = table.insertRow(i);
+        for (let j = 0; j < width; j++) { //Defining the width of the grid
+            const cell = row.insertCell(j);
+            cell.onclick = () => {//event listener for onclick
+                cell.style.backgroundColor = colorPicker.value;//Setting the choosen color value to the grid
+            };
+        }
     }
-  }
-  pixelCanvas.html(grid);
-  pixelCanvas.css("background-color", "#ffffff");//setting background color for canvas
 };
 
 const sizePicker = document.querySelector("#sizePicker");
 sizePicker.addEventListener("submit", function(evt) {
-  var width = $('#input_width').val();
-  var height = $('#input_height').val();
-    evt.preventDefault();
+    while (table.rows.length > 0) {
+        table.deleteRow(0);//Deleting the exsiting grid when pressed on submit button
+    }
+    var width = $('#input_width').val();//Usser defined width
+    var height = $('#input_height').val();//User defined height 
+    evt.preventDefault();//Calling method not to triiger the deafult action
     makeGrid(width, height);
 });
